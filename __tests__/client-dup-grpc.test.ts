76c98a4f5b3e0db92188faeaa8922f5ea5387478
edvardchen <>
2019-08-18 11:59:10 +0800

    test: fix grpc dep resolution error

--- __tests__/client-dup-grpc.test.ts
+++ __tests__/client-dup-grpc.test.ts
@@ -3,29 +3,34 @@ import { exec } from 'child_process';
 import fs from 'fs';
 
 describe('duplicate grpc', () => {
-  const fakeProject = __dirname + '/fixtures/helloworld/static_codegen';
+  const fakeProject = __dirname + '/fixtures/fakeProject';
   beforeAll(done => {
-    fs.writeFile(fakeProject + '/package.json', '{}', error => {
+    exec(`mkdir ${fakeProject}`, error => {
       if (error) return done(error);
-      exec('npm i grpc', { env: process.env, cwd: fakeProject }, done);
+      fs.writeFile(fakeProject + '/package.json', '{}', error => {
+        if (error) return done(error);
+        exec('npm i grpc', { env: process.env, cwd: fakeProject }, done);
+      });
     });
   }, 60000);
 
   afterAll(done => {
-    exec(
-      `rm -rf ${fakeProject}/node_modules ${fakeProject}/package.json`,
-      done
-    );
+    exec(`rm -rf ${fakeProject}`, error => {
+      done(error);
+    });
   });
 
   it('method call should throw error', () => {
-    jest.resetModules();
     const { credentials } = require('grpc');
+    jest.doMock('grpc', () => {
+      return require(`${fakeProject}/node_modules/grpc`);
+    });
     const {
       GreeterClient,
     } = require('./fixtures/helloworld/static_codegen/helloworld_grpc_pb');
     expect(() => {
       new GreeterClient('localhost:1234', credentials.createInsecure());
     }).toThrow("Channel's second argument must be a ChannelCredentials");
+    jest.unmock('grpc');
   });
 });
